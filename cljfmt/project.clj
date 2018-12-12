175785789958437d58ace6f246e808a81ca4f544
Peter Strömberg <pez@pezius.com>
2018-12-12 10:32:44 +0100

    Bump version

--- cljfmt/project.clj
+++ cljfmt/project.clj
@@ -1,4 +1,4 @@
-(defproject pez/cljfmt "0.0.1-SNAPSHOT"
+(defproject pez/cljfmt "0.0.2-SNAPSHOT"
   :description "An adapted cljfmt used by Calva Formatter"
   :url "https://github.com/PEZ/cljfmt"
   :scm {:dir ".."}
